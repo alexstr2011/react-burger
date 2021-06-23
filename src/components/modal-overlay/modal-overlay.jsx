4179590a9c345859e3e0c3f186069bed533be062
Alexey <alexstranger2011@yandex.ru>
2021-06-23 23:36:01 +0700

    Remove portal from ModalOverlay

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import styles from './modal-overlay.module.css';
 
 function ModalOverlay({closeModal}) {
-    return ReactDOM.createPortal(
-        (
-            <div onClick={closeModal} className={styles.overlay}>
-            </div>
-        ),
-        document.getElementById("react-modals")
+    return (
+        <div onClick={closeModal} className={styles.overlay} />
     );
 }
 
